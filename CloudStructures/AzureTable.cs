beca8aeef06e63c98d44ddbb25ea4e6425bc1e28
RamonGiovane <ramon.giovane@gmail.com>
2020-05-08 15:45:31 -0300

    [FIX] Missing line command in AzureTable

--- CloudStructures/AzureTable.cs
+++ CloudStructures/AzureTable.cs
@@ -3,7 +3,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 /// <summary>
