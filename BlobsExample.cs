514f9c5ca0a42e711c14149df361d313599a6725
RamonGiovane <ramon.giovane@gmail.com>
2020-08-13 13:04:14 -0300

    [ADD] Azure Blobs and examples

---
+++ BlobsExample.cs
@@ -0,0 +1,48 @@
+﻿using Rgd.AzureAbstractions.CloudStructures;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Rgd.AzureAbstractions.Tests
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+
+            new Tester().UsingAzureBlobs().GetAwaiter().GetResult();
+
+        }
+
+        class Tester
+        {
+            public async Task UsingAzureBlobs()
+            {
+                //Using the environment variable 'AzureWebJobsStorage' for storage authentication
+                //AzureBlobs blobs = new AzureBlobs("samplecontainer");
+
+                //Using literal connection string for authentication
+                AzureBlobs blobs = new AzureBlobs("samplecontainer", "my_connection_string", null);
+
+                //Creating "samplecontainer". Loading it if already exists.
+                blobs.CreateOrLoadStructure();
+
+                //Set this true to deactivate inner logs.
+                blobs.LoggingDisabled = false;
+
+                //Sending a blob to the container from an absolute path
+                await blobs.UploadBlob("C:\\Users\\foo\\Desktop\\image.png", "wallpaper.png");
+
+                //Sending a blob to the container from an relative path. In this case the name can be the same for both
+                await blobs.UploadBlob("reports.json");
+
+                //Downloading a blob as Strean
+                Stream stream = await blobs.DownloadBlobAsStream("wallpaper.png");
+
+                //Downloading a blob as a local file
+                await blobs.DownloadBlobAsFile("wallpaper.png", "C:\\Users\\foo\\Desktop\\image-copy.png");
+
+
+            }
+        }
+    }
+}
---
+++ CloudStructures/AzureBlobs.cs
@@ -0,0 +1,200 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+/// <summary>
+///     2020 - Version 1.4.0 
+///     Developed by: Ramon Giovane Dias
+/// </summary>
+namespace Rgd.AzureAbstractions.CloudStructures
+{
+    /// <summary>
+    ///  Manipulates a blob container from the Azure storage.
+    ///  An instance of this class can only handle one container, which must be specified by it's name on the constructor.
+    ///  With this class is possible to create, load or delete a container, and upload to, download or delete binary data from a container.
+    /// </summary>
+    public class AzureBlobs : CloudStructure
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public AzureBlobs(string containerName) : this(containerName, null) { }
+
+        public AzureBlobs(string containerName, ILogger logger) : base(containerName, logger)
+        {
+            _blobServiceClient = new BlobServiceClient(ConnectionString);
+        }
+
+        public AzureBlobs(string containerName, string connectionString, ILogger logger) : base(containerName, connectionString, logger)
+        {
+            _blobServiceClient = new BlobServiceClient(connectionString);
+        }
+
+        public override bool CreateOrLoadStructure()
+        {
+            if (!IsCreated())
+            {
+
+                return LogAzureResponse("Create Container", Task.Run(
+                    () => _blobServiceClient.CreateBlobContainerAsync(StructureName)).Result.GetRawResponse());
+            }
+            return true;
+        }
+
+        public override bool IsCreated()
+        {
+
+            return Task.Run(() => _blobServiceClient.GetBlobContainerClient(StructureName).ExistsAsync()).Result;
+
+        }
+
+        private BlobClient CreateBlobObject(string blobName)
+        {
+            return new BlobClient(ConnectionString, StructureName, blobName);
+        }
+
+        public override bool DeleteStructure()
+        {
+            if (!IsCreated()) return false;
+            return LogAzureResponse("Delete Container",
+                Task.Run(() => _blobServiceClient.GetBlobContainerClient(StructureName).DeleteAsync()).Result);
+        }
+
+
+        /// <summary>
+        ///     Uploads a local file from the current file system to the cloud blob container.
+        ///     <br></br>
+        ///     If the parameter <paramref name="fileNameOnStorage"/> is negligenciated, it will be assumed as the same value
+        ///     of the given <paramref name="filePath"/>.
+        ///     <br></br><br></br>
+        ///     
+        ///     
+        /// 
+        ///     Warning: using the default value of <paramref name="fileNameOnStorage"/> and an absolute path in <paramref name="filePath"/>
+        ///     can be dangerous.
+        ///     <br></br><br></br>
+        ///     Warning: using a relative path in <paramref name="filePath"/> can be dangerous while executing in the Azure environment
+        ///     
+        ///     
+        /// </summary>
+        /// <param name="filePath">The file to be uploaded from the current file system.</param>
+        /// <returns></returns>
+        /// <param name="stream">The file to be uploaded as stream</param>
+        /// <param name="fileNameOnStorage">The name of the new file to be uploaded in the container</param>
+        public async Task<bool> UploadBlob(string filePath, string fileNameOnStorage = null)
+        {
+            try
+            {
+                var blob = CreateBlobObject(fileNameOnStorage ?? filePath);
+
+                if (await blob.ExistsAsync())
+                {
+                    TryLog($"{StructureName}: {fileNameOnStorage ?? filePath} already exists.");
+                    return true;
+                }
+
+                var response = await blob.UploadAsync(filePath);
+
+                return LogAzureResponse("Upload Blob", response.GetRawResponse());
+            }
+            catch (Exception e)
+            {
+                TryLogError($"{StructureName}: Error attempting to upload: {fileNameOnStorage} - {e.Message}");
+                return false;
+            }
+
+        }
+
+
+        /// <summary>
+        ///     Uploads a local file from the current file system to the cloud blob container.
+        ///     <br></br><br></br>
+        ///     Warning: using relative filepaths to file to be uploaded can be dangerous while executing in the Azure environment.
+        /// </summary>
+        /// <returns></returns>
+        /// <param name="stream">The file to be uploaded as stream</param>
+        /// <param name="fileNameOnStorage">The name of the new file to be uploaded in the container</param>
+        /// <returns></returns>
+        public async Task<bool> UploadBlob(Stream stream, string fileNameOnStorage)
+        {
+            try
+            {
+                var blobClient = CreateBlobObject(fileNameOnStorage);
+
+                if (await blobClient.ExistsAsync())
+                {
+                    TryLog($"{StructureName}: {fileNameOnStorage} already exists.");
+                    return true;
+                }
+
+                var response = (await blobClient.UploadAsync(stream)).GetRawResponse();
+
+                return LogAzureResponse("Upload Blob", response);
+            }
+            catch (Exception e)
+            {
+                TryLogError($"{StructureName}: Error attempting to upload: {fileNameOnStorage} - {e.Message}");
+                return false;
+            }
+
+        }
+
+        public async Task<MemoryStream> DownloadBlobAsStream(string blobName)
+        {
+            var blob = CreateBlobObject(blobName);
+            var ms = new MemoryStream();
+
+            try
+            {
+                LogAzureResponse("Download Blob", await blob.DownloadToAsync(ms));
+
+                ms.Position = 0;
+
+                return ms;
+            }
+            catch (Exception e)
+            {
+                TryLogError($"{StructureName}: Error attempting to download: {blobName} - {e.Message}");
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        ///     Donwloads a blob file from the container and saves it in a local file in the current file system.
+        ///     <br></br><br></br>
+        ///     Warning: using relative filepaths to new file to be created can be dangerous while executing in the Azure environment.
+        /// </summary>
+        /// <param name="blobName">The name of the file in the container</param>
+        /// <param name="filePathLocally">The full name (filepath) of the file to be created on the current file system.</param>
+        /// <returns></returns>
+        public async Task<string> DownloadBlobAsFile(string blobName, string filePathLocally)
+        {
+            try
+            {
+                var blob = CreateBlobObject(blobName);
+
+                if (LogAzureResponse("Download Blob to File", await blob.DownloadToAsync(filePathLocally)))
+                    return filePathLocally;
+            }
+            catch (Exception e)
+            {
+                TryLogError($"{StructureName}: Error attempting to download: {blobName} - {e.Message}");
+            }
+            return null;
+        }
+
+        public bool DeleteBlob(string blobName)
+        {
+            return LogAzureResponse("Delete Blob", CreateBlobObject(blobName).Delete());
+        }
+
+        public async Task<bool> DeleteBlobAsync(string blobName)
+        {
+            return LogAzureResponse("Delete Blob", await CreateBlobObject(blobName).DeleteAsync());
+        }
+
+
+    }
+}
